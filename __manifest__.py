f6e1ba644b90499dedea46bbe6be4acea36ed855
Walther Barnett <walther.barnett@gmail.com>
2026-01-06 10:32:19 +0100

    prob with views

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'MobilePay Payment Provider',
-    'version': '17.0.1.0.0',
-    'category': 'Accounting/Payment Providers',
-    'summary': 'Payment Provider: MobilePay (Vipps) for Danish market',
-    'description': """
+    "name": "MobilePay Payment Provider",
+    "version": "17.0.1.0.0",
+    "category": "Accounting/Payment Providers",
+    "summary": "Payment Provider: MobilePay (Vipps) for Danish market",
+    "description": """
 MobilePay Payment Provider for Odoo 17
 
 This module integrates Vipps MobilePay ePayment API v3 with Odoo, providing:
@@ -17,29 +17,29 @@ This module integrates Vipps MobilePay ePayment API v3 with Odoo, providing:
 
 Supports both Odoo Enterprise and Community editions.
     """,
-    'author': 'Odoo Community',
-    'website': 'https://github.com/OCA/payment',
-    'license': 'LGPL-3',
-    'depends': [
-        'payment',
-        'account',
-        'website_sale',
+    "author": "Odoo Community",
+    "website": "https://github.com/OCA/payment",
+    "license": "LGPL-3",
+    "depends": [
+        "payment",
+        "account",
+        "website_sale",
     ],
-    'data': [
-        'views/payment_mobilepay_templates.xml',
-        'data/payment_provider_data.xml',
-        'views/payment_provider_views.xml',
-        'views/payment_transaction_views.xml',
+    "data": [
+        "views/payment_mobilepay_templates.xml",
+        "data/payment_provider_data.xml",
+        "views/payment_provider_views.xml",
+        "views/payment_transaction_views.xml",
     ],
-    'assets': {
-        'web.assets_frontend': [
-            'mobilepay_payment/static/src/js/payment_form.js',
+    "assets": {
+        "web.assets_frontend": [
+            "mobilepay_payment/static/src/js/payment_form.js",
         ],
     },
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    'external_dependencies': {
-        'python': ['requests', 'cryptography'],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "external_dependencies": {
+        "python": ["requests", "cryptography"],
     },
 }
